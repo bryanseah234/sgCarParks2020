a8de8aa1373c06eea0fce9d08bd1cbb23d38f90b
JS Ng <ng_jun_siang@moe.edu.sg>
2020-08-19 00:02:08 +0000

    removed sample implementation

--- classes.py
+++ classes.py
@@ -1,25 +1,2 @@
 # Task 1.2
-class ContactList:
-    def __init__(self, size):
-        self.size = size
-        self.data = [None] * self.size
-    
-    @staticmethod
-    def hash(inputstr):
-        total = 0
-        for pnum in range(len(inputstr)):
-            char = inputstr[pnum]
-            cnum = ord(char)
-            total += (pnum * cnum)
-        return total
-    
-    def add(self, key, value):
-        index = self.hash(key) % self.size
-        if self.data[index] is None:
-            self.data[index] = {key: value}
-        else:
-            self.data[index][key] = value
-
-    def get(self, key):
-        index = self.hash(key) % self.size
-        return self.data[index][key]
+# Write your code here
