d936b95723c9311480385f1b2b8b5592a67b1656
JS Ng <ng_jun_siang@moe.edu.sg>
2020-08-24 09:25:33 +0000

    updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from classes import ContactList
-
-# https://repl.it/@mrjsng/ExamPractice1
+# https://repl.it/@mrjsng/ExamPractice2
 
 # Task 1.1
 # Write your code here
 
+# Task 1.2
+# Write your code here
+
 # Task 1.3
+# Write your code here
+
+# Task 1.4
+# Write your code here
+
+# Task 1.5
 # Write your code here
